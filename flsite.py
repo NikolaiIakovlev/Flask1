7dd93969f298e94901e52709ec1d48fa3287936d
NikolaiIakovlev <118279320+NikolaiIakovlev@users.noreply.github.com>
2023-08-17 18:34:46 +0300

    подключили sqlite

--- flsite.py
+++ flsite.py
@@ -1,18 +1,60 @@
+import sqlite3
+import os
 from flask import (
-    Flask, render_template, url_for, request, flash, session, redirect, abort
+    Flask, render_template, url_for, request, flash, session,
+    redirect, abort, g
     )
 
+# конфигурация БД
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+USERNAME = 'admin'
+PASSWORD = '123'
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+
+
+def connect_db():
+    """БД"""
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
         {"name": "Обратная связь", "url": "contact"}]
 
 
+def get_db():
+    '''Соединение с БД, если оно еще не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 @app.route("/index")
 @app.route("/")
 def index():
+    db = get_db()
     print(url_for('index'))
     return render_template('index.html', menu=menu)
 
---
+++ sq_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS mainmenu (
+id integer PRIMARY KEY autoincrement,
+title text NOT NULL,
+url text NOT NULL
+);
