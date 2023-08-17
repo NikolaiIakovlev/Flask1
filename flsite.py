3c76784fb63940d3991c0e84e52a2bdcce0c263d
NikolaiIakovlev <118279320+NikolaiIakovlev@users.noreply.github.com>
2023-08-17 11:32:05 +0300

    loggin

--- flsite.py
+++ flsite.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import (
+    Flask, render_template, url_for, request, flash, session, redirect, abort
+    )
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fdgdfgdfggf786hfg6hfg6h7f'
@@ -15,9 +17,9 @@ def index():
     return render_template('index.html', menu=menu)
 
 
-@app.route("/profile/<username>")
-def profile(username):
-    return f"Пользователь: {username}"
+# @app.route("/profile/<username>")
+# def profile(username):
+#     return f"Пользователь: {username}"
 
 
 @app.route("/about")
@@ -25,6 +27,15 @@ def about():
     print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+        
+    return render_template('login.html', title="Авторизация", menu=menu)
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
@@ -35,6 +46,17 @@ def contact():
             flash('Ошибка отправки', category='error')
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+ 
+    return f"Пользователь: {username}"
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 # with app.test_request_context():
 #     print(url_for('about'))
