b8fef67daf1f019f13157738b5d79fbe7f9111db
NikolaiIakovlev <118279320+NikolaiIakovlev@users.noreply.github.com>
2023-08-19 10:28:24 +0300

    sql2

--- flsite.py
+++ flsite.py
@@ -1,11 +1,9 @@
 import sqlite3
 import os
-from flask import (
-    Flask, render_template, url_for, request, flash, session,
-    redirect, abort, g
-    )
+from flask import Flask, render_template, request, g, flash, abort
+from FDataBase import FDataBase
 
-# конфигурация БД
+# конфигурация
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
@@ -14,12 +12,10 @@ PASSWORD = '123'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
 
 
 def connect_db():
-    """БД"""
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
@@ -34,75 +30,53 @@ def create_db():
     db.close()
 
 
-menu = [{"name": "Установка", "url": "install-flask"},
-        {"name": "Первое приложение", "url": "first-app"},
-        {"name": "Обратная связь", "url": "contact"}]
-
-
 def get_db():
-    '''Соединение с БД, если оно еще не установлено'''
-    if not hasattr(g, 'link_db'):
-        g.link_db = connect_db()
-    return g.link_db
+    """Соединение с базой данных, если оно еще не установлено"""
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db
 
 @app.teardown_appcontext
 def close_db(error):
-    '''Закрываем соединение с БД, если оно было установлено'''
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
+    """Закрытие соединения с базой данных"""
+    if hasattr(g, 'db'):
+        g.db.close()
 
-@app.route("/index")
 @app.route("/")
 def index():
     db = get_db()
-    print(url_for('index'))
-    return render_template('index.html', menu=menu)
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
 
 
-# @app.route("/profile/<username>")
-# def profile(username):
-#     return f"Пользователь: {username}"
-
-
-@app.route("/about")
-def about():
-    print(url_for('about'))
-    return render_template('about.html', title="О сайте", menu=menu)
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-        
-    return render_template('login.html', title="Авторизация", menu=menu)
-
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    if request.method == "POST":
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='error')
-    return render_template('contact.html', title="Обратная связь", menu=menu)
-
-
-@app.route("/profile/<username>")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(401)
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
  
-    return f"Пользователь: {username}"
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category = 'error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+ 
+    return render_template('add_post.html', menu = dbase.getMenu(), title="Добавление статьи")
 
-@app.errorhandler(404)
-def pageNotFount(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
-# with app.test_request_context():
-#     print(url_for('about'))
-#     print(url_for("profile", username="ypoura"))
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+ 
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
